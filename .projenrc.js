262103b7242a09283c4eee8aaddf3b7e9ad7d506
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2022-07-29 08:35:31 -0700

    fix: projen cleanups

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,16 @@
 const { clickupCdk } = require('@time-loop/clickup-projen');
+
+const bundledDeps = [];
+const peerDeps = [];
+
 const project = new clickupCdk.ClickUpCdkConstructLibrary({
   name: '@time-loop/cdk-documentdb',
-  author: 'DevOps Team',
+
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  deps: ['multi-convention-namer'],
-  devDeps: ['@time-loop/clickup-projen'],
+  licensed: true,
+
+  deps: [...bundledDeps, 'multi-convention-namer'],
+  devDeps: [...peerDeps, '@time-loop/clickup-projen'],
 });
 project.synth();
